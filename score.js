7b501ff3e5605da9cb1312d04d113d60c0efc5cd
Gator2020 <65585022+Gator2020@users.noreply.github.com>
2021-03-17 22:41:28 -0500

    Add files via upload

---
+++ score.js
@@ -0,0 +1,20 @@
+      var Death = 0;
+      var Points =  0;
+      function AddDeath(DeathData)
+         {
+             //Death Increments after each time function is called based on the behavior of the user
+DeathData = this.Death++;
+
+var ScoreCount = function Score(Pointdata)
+{
+
+   ScoreCount = this.Death++;
+ ScoreCount = this.document.getElementById("Lives");
+}
+var DeathCount = function Death(DeathData)
+ {
+    DeathCount = this.Points++;
+    DeathCount = this.document.getElementById("Loss");
+}
+        
+}
